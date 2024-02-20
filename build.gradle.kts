74c5134c26d3ac2ceebcf69bc5aa6dfec6c63fb1
Gabriel Lee <chihonglee777@gmail.com>
2024-02-20 11:22:56 +0000

    release: 0.1.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "dev.gabrielchl"
-version = "0.1.4"
+version = "0.1.5"
 
 repositories {
     mavenCentral()
@@ -28,7 +28,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("221")
-        untilBuild.set("233.*")
+        untilBuild.set("241.*")
     }
 
     signPlugin {

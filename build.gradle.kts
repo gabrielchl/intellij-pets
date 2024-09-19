20d059f67ea7aad86f0ad0337872eac1741d046e
Gabriel Lee <chihonglee777@gmail.com>
2024-09-19 19:46:49 +0100

    release: 0.1.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2024.2.1")
+  version.set("2023.2.6")
   type.set("IC") // Target IDE Platform
 
   plugins.set(listOf(/* Plugin Dependencies */))
@@ -32,7 +32,7 @@ tasks {
 
   patchPluginXml {
     sinceBuild.set("232")
-    untilBuild.set("243.*")
+    untilBuild.set("")
   }
 
   signPlugin {
